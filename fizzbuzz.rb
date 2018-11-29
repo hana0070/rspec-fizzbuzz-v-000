0bee32b502486226da8dfa5fd3f8f327131336c4
HANA LEANDER <hana0070@gmail.com>
2018-11-29 20:44:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz()
+  if 3 
+    puts "Fizz"
+  elsif 5
+    puts "Buzz"
+  elseif 15 
+    puts "Fizzbuzz"
+  else
+    
+  end
+end
